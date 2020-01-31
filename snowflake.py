5c9e7701a059eaa009f9d2c7031a1dcc6b37356d
Coby Lu <coby.lu@appian.com>
2020-01-31 15:46:21 -0500

    use pulp for linear programming solver

--- snowflake.py
+++ snowflake.py
@@ -2,6 +2,7 @@ from itertools import combinations
 
 import numpy as np
 import pandas as pd
+from pulp import *
 from scipy.optimize import linprog
 import spacy
 
@@ -36,46 +37,48 @@ def optimize(similarity):
     num_people = similarity.shape[0]
     print(f"num people1: {num_people}")
 
-    c = np.reshape(similarity, -1)
+    lp_vars = []
+    c = []
     for i in range(num_people):
         for j in range(num_people):
-            c[i * num_people + j] = c[i * num_people + j] if i != j else 99999
+            lp_vars.append(LpVariable(f"g_{i}_r_{j}", lowBound = 0, upBound = 1, cat=LpInteger))
+            c.append(similarity[i][j] if i != j else 99999)
     
-    # give to only 1
-    give = np.zeros((num_people, num_people**2))
-    for i in range(num_people):
-        give[i, i * num_people:(i + 1) * num_people] = 1
+    prob = LpProblem("snowflake", LpMinimize)
     
-    # receive from only 1
-    receive = np.zeros((num_people, num_people**2))
+    # minimization function
+    prob += lpDot(c, lp_vars)
+
+    # give only 1
     for i in range(num_people):
-        receive[i, i:num_people ** 2:num_people] = 1
+        prob += lpSum([lp_vars[i] for i in range(i * num_people, (i + 1) * num_people)]) == 1
 
-    # don't give and receive from same person
-    giv_rec = np.zeros((num_people * (num_people - 1) // 2, num_people**2))
-    for i, comb in enumerate(combinations(range(num_people), 2)):
-        giv_rec[i, comb[0] * num_people + comb[1]] = 1
-        giv_rec[i, comb[1] * num_people + comb[0]] = 1
+    # receive only 1
+    for i in range(num_people):
+        prob += lpSum([lp_vars[i] for i in range(i, num_people ** 2, num_people)]) == 1
 
-    A_eq = np.vstack((give, receive))
-    b_eq = np.ones(2 * num_people)
-
-    A_ub = giv_rec
-    b_ub = np.ones((num_people - 1) * num_people // 2)
-    return linprog(c, A_eq=A_eq, b_eq=b_eq, A_ub=A_ub, b_ub=b_ub, bounds=(0, 1), method="revised simplex")
+    # don't give and receive to the same person
+    for comb in combinations(range(num_people), 2):
+        prob += lp_vars[comb[0] * num_people + comb[1]] + lp_vars[comb[1] * num_people + comb[0]] <= 1
+    
+    return lp_vars, prob.solve()
 
 if __name__ == "__main__":
     df = pd.read_csv("Engineering Secret Snowflake 2019 (Responses) - Form Responses 1.csv")
     going = df.loc[df["Do you want to participate in Engineering's Secret Snowflake?"] == "Yes", ["Email Address", "List 4 facts about yourself to help your secret snowflake find the perfect gift for you! "]].reset_index(drop=True)
 
     interest_similarity = similarity(going)
-    result = optimize(interest_similarity)
+    lp_vars, result = optimize(interest_similarity)
 
-    solution = np.round(result.x)
+    print(LpStatus[result])
     members = going["Email Address"]
     num_people = len(members)
-    print(f"num people2: {num_people}")
+    check = 0
+    matching = pd.DataFrame(columns = ["Giver", "Receiver", "Giver's interests", "Receiver's Interests"])
     for i, g in enumerate(members):
         for j, r in enumerate(members):
-            if solution[i * num_people + j] == 1:
-                print(f"{g} gives to {r}")
+            if lp_vars[i * num_people + j].varValue  >= 0.95:
+                matching = matching.append({"Giver": g, "Receiver": r, "Giver's interests": going.loc[i, "List 4 facts about yourself to help your secret snowflake find the perfect gift for you! "], "Receiver's Interests": going.loc[j, "List 4 facts about yourself to help your secret snowflake find the perfect gift for you! "]}, ignore_index = True)
+                check += 1
+    matching.to_csv("matching.csv",index = False, quoting=true)
+    print(f"{check} number of snowflakes matched")
